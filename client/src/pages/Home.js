5fd4ad2233772c1c794f5432f14163390a4b30fe
Penguuu <pengvang95@yahoo.com>
2023-08-17 11:24:53 -0500

    Home page login/sign up, and profile

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,135 @@
-export default function Home () {
-    return <div class="home">
-                <h1 className="text-3xl font-bold underline">
-                    RiffSync Home
-                </h1>
-           </div>
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import fakeAccounts from '../components/fakeAccounts';
+import '../components/resources/home.css';
+
+export default function Home() {
+  const [signupEmail, setSignupEmail] = useState('');
+  const [signupPassword, setSignupPassword] = useState('');
+  const [signupName, setSignupName] = useState('');
+  const [instrument, setInstrument] = useState('');
+  const [subcategory, setSubcategory] = useState('');
+  const [genre, setGenre] = useState('');
+  const [skillLevel, setSkillLevel] = useState('Beginner');
+
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSignupSubmit = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      id: fakeAccounts.length + 1,
+      name: signupName,
+      email: signupEmail,
+      password: signupPassword,
+      instrument: instrument,
+      subcategory: subcategory,
+      genre: genre,
+      skillLevel: skillLevel,
+    };
+
+    fakeAccounts.push(newUser);
+
+    setSignupName('');
+    setSignupEmail('');
+    setSignupPassword('');
+    setInstrument('');
+    setSubcategory('');
+    setGenre('');
+    setSkillLevel('Beginner');
+
+    console.log('User signed up:', newUser);
+
+    localStorage.setItem('user', JSON.stringify(newUser));
+    navigate('/profile');
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    const user = fakeAccounts.find(account => account.email === loginEmail && account.password === loginPassword);
+
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      navigate('/profile');
+    } else {
+      alert('Invalid login credentials');
+    }
+  };
+
+
+  return (
+    <div className="home">
+      <h1 className="text-3xl font-bold underline">RiffSync Home</h1>
+      <div className="container">
+        <h2 className="signup-title">Signup</h2>
+        <form onSubmit={handleSignupSubmit} className="form-space">
+          <label>
+            Name:
+            <input type="text" value={signupName} onChange={(e) => setSignupName(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type="text" value={signupEmail} onChange={(e) => setSignupEmail(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="text" value={signupPassword} onChange={(e) => setSignupPassword(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Instrument:
+            <input type="text" value={instrument} onChange={(e) => setInstrument(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Subcategory:
+            <input type="text" value={subcategory} onChange={(e) => setSubcategory(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Genre:
+            <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Skill Level:
+            <select value={skillLevel} onChange={(e) => setSkillLevel(e.target.value)}>
+              <option value="Beginner">Beginner</option>
+              <option value="Intermediate">Intermediate</option>
+              <option value="Expert">Expert</option>
+              <option value="Professional">Professional</option>
+              <option value="God Tier">God Tier</option>
+            </select>
+          </label>
+          <br />
+          <button type="submit" className="submit-button">Signup</button>
+      </form>
+    </div>
+
+
+    <div className="container">
+        <h2 className="signup-title">Login</h2>
+        <form onSubmit={handleLoginSubmit} className="form-space">
+          <label>
+            Email:
+            <input type="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} className="input-field" />
+          </label>
+          <br />
+          <button type="submit" className="submit-button">Login</button>
+        </form>
+      </div>
+
+      <Link to="/search" className="link">Go to Search Page</Link>
+    </div>
+  );
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,29 @@
-export default function Home () {
-    return <div class="home">
-                <h1 className="text-3xl font-bold underline">
-                    RiffSync Profile
-                </h1>
-           </div>
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Profile() {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('user'));
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    navigate('/');
+  };
+
+  return (
+    <div className="profile">
+      <h1 className="text-3xl font-bold underline">RiffSync Profile</h1>
+      {user ? (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Instrument: {user.instrument}</p>
+          <p>Subcategory: {user.subcategory}</p>
+          <p>Genre: {user.genre}</p>
+          <p>Skill Level: {user.skillLevel}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <p>No user information found.</p>
+      )}
+    </div>
+  );
 }
