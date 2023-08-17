02405f01c07ac90b18b547cff8b3fe3fee856ec2
Jrorem <joerorem@gmail.com>
2023-08-17 16:19:48 -0500

    integrated more features with current styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,22 +1,32 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Navigation() {
 
     const router = useLocation();
     return (
+    //     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    //     <Navbar bg="dark" data-bs-theme="dark">
+    //     <Container>
+    //       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    //       <Nav className="me-auto">
+    //         <Nav.Link href="#home">Home</Nav.Link>
+    //         <Nav.Link href="#features">Features</Nav.Link>
+    //         <Nav.Link href="#pricing">Pricing</Nav.Link>
+    //       </Nav>
+    //     </Container>
+    //   </Navbar>
+    //   </link>
         <div class="navigate">
             <nav>
                 <ul class="navbar">
 
                     <Button variant="secondary">
-                        <NavLink
-                            className={router.pathname === '/' ? 'active' : ''} to='/'
-                            // className={({isActive}) => {
-                            // return (  
-                            // (isActive ? 'secondary' : 'primary')
-                            //     )
-                            // }}
+                        <NavLink to='/' style={{ textDecoration: 'none' }}
+                            className={router.pathname === '/' ? 'active' : ''}
                         >
                             Home
                         </NavLink>
@@ -24,7 +34,7 @@ export default function Navigation() {
 
 
                     <Button variant="secondary">
-                        <NavLink to='/jammers'
+                        <NavLink to='/jammers' style={{ textDecoration: 'none' }}
                             className={router.pathname === '/jammers' ? 'active' : ''}
 
                         >
@@ -34,7 +44,7 @@ export default function Navigation() {
 
 
                     <Button variant="secondary">
-                        <NavLink to='/profile'
+                        <NavLink to='/profile' style={{ textDecoration: 'none' }}
                             className={router.pathname === '/profile' ? 'active' : ''}
 
                         >
@@ -44,7 +54,7 @@ export default function Navigation() {
 
 
                     <Button variant="secondary">
-                        <NavLink to='/Resources'
+                        <NavLink to='/Resources' style={{ textDecoration: 'none', }}
                             className={router.pathname === '/Resources' ? 'active' : ''}
 
                         >
@@ -54,7 +64,7 @@ export default function Navigation() {
 
 
                     <Button variant="secondary">
-                        <NavLink to='/Search'
+                        <NavLink to='/Search' style={{ textDecoration: 'none' }}
                             className={router.pathname === '/Search' ? 'active' : ''}
 
                         >
@@ -63,13 +73,13 @@ export default function Navigation() {
                     </Button>
 
                     <Button variant="secondary">
-  <NavLink 
-    to='/theTeam'
-    className={router.pathname === '/theTeam' ? 'active' : ''}
-  >
-    The Team
-  </NavLink>
-</Button>
+                        <NavLink
+                            to='/theTeam' style={{ textDecoration: 'none' }}
+                            className={router.pathname === '/theTeam' ? 'active' : ''}
+                        >
+                            The Team
+                        </NavLink>
+                    </Button>
 
 
                 </ul>
